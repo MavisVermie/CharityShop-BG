dcebec8ee6c799eaa0ae6ad169e3a7a33526241d
MavisVermie <127209916+MavisVermie@users.noreply.github.com>
2025-04-03 22:32:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const port = process.env.PORT || 8000
 const express = require('express')
 const environment =  "production"
 const config = require('./config/config')
+
 require('./config/database.config')({
   connectionString: process.env.MONGODB_URI
 })
+
 const app = express()
 
-database(config[environment])
 require('./config/express')(app, config[environment])
 require('./config/routes')(app)
 require('./config/passport')()
 
-app.listen(port, () => { console.log('Listening on port: ' + port) })
+app.listen(port, () => {
+  console.log('Listening on port: ' + port)
+})
